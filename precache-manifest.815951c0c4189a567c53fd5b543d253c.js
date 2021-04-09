c528c1f4f621196cbf6c5164369e941a1a86909c
patrickhennessy-dal <80766219+patrickhennessy-dal@users.noreply.github.com>
2021-04-09 13:50:33 -0300

    Updates

---
+++ precache-manifest.815951c0c4189a567c53fd5b543d253c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9dba2a41c595ab7827c56d4a31afe3",
+    "url": "/ReactComputerVisionTemplate/index.html"
+  },
+  {
+    "revision": "933f18d3751f7eb0f0b3",
+    "url": "/ReactComputerVisionTemplate/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "44e0e9243c2d98d0f069",
+    "url": "/ReactComputerVisionTemplate/static/js/2.b13e1942.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/ReactComputerVisionTemplate/static/js/2.b13e1942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "933f18d3751f7eb0f0b3",
+    "url": "/ReactComputerVisionTemplate/static/js/main.1ae43efc.chunk.js"
+  },
+  {
+    "revision": "60a90b6bf66b019d2b84",
+    "url": "/ReactComputerVisionTemplate/static/js/runtime-main.db29c028.js"
+  }
+]);
